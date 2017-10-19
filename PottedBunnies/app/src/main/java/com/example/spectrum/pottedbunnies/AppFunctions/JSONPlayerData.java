17be0addf78fae2bfc8fc003b08ce1829cfae212
Alvin <apyan7852@gmail.com>
2017-10-18 20:24:32 -0700

    Graphics
    Graphics has been uploaded!

---
+++ PottedBunnies/app/src/main/java/com/example/spectrum/pottedbunnies/AppFunctions/JSONPlayerData.java
@@ -0,0 +1,8 @@
+package com.example.spectrum.pottedbunnies.AppFunctions;
+
+/**
+ * Created by apyan on 10/18/17.
+ */
+
+public class JSONPlayerData {
+}
---
+++ PottedBunnies/app/src/main/java/com/example/spectrum/pottedbunnies/GameOrderPluckingScreen.java
@@ -0,0 +1,13 @@
+package com.example.spectrum.pottedbunnies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameOrderPluckingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_order_plucking_screen);
+    }
+}
