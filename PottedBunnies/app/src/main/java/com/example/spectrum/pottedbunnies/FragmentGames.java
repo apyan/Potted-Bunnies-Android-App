3049e016a8a563773d8472b96549205883495746
Alvin <apyan7852@gmail.com>
2017-10-20 03:09:31 -0700

    Testing App
    Fragments are more developed.
    Need more graphics though.
    More data and strings are developed.

--- PottedBunnies/app/src/main/java/com/example/spectrum/pottedbunnies/FragmentGames.java
+++ PottedBunnies/app/src/main/java/com/example/spectrum/pottedbunnies/FragmentGames.java
@@ -1,10 +1,12 @@
 package com.example.spectrum.pottedbunnies;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.spectrum.pottedbunnies.AppFunctions.AppGraphics;
 
@@ -16,6 +18,7 @@ public class FragmentGames extends Fragment implements View.OnClickListener {
 
     // Variables for Activity Fragment
     AppGraphics activityFragGraphics;
+    Button button_00, button_01;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -23,17 +26,28 @@ public class FragmentGames extends Fragment implements View.OnClickListener {
         View v = inflater.inflate(R.layout.fragment_games, container, false);
         activityFragGraphics = new AppGraphics(getActivity());
 
+        // Assigning UI attributes
+        button_00 = (Button) v.findViewById(R.id.games_00);
+        button_00.setOnClickListener(this);
+        button_00.setTextColor(Color.WHITE);
+        button_01 = (Button) v.findViewById(R.id.games_01);
+        button_01.setOnClickListener(this);
+        button_01.setTextColor(Color.WHITE);
+
         return v;
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            /*
-            case R.id.citizen_button:
+            // To Matching Pots game
+            case R.id.games_00:
 
                 break;
-                */
+            // To Order Plucking game
+            case R.id.games_01:
+
+                break;
             default:
                 break;
         }
--- PottedBunnies/app/src/main/java/com/example/spectrum/pottedbunnies/FragmentStore.java
+++ PottedBunnies/app/src/main/java/com/example/spectrum/pottedbunnies/FragmentStore.java
@@ -1,10 +1,12 @@
 package com.example.spectrum.pottedbunnies;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.spectrum.pottedbunnies.AppFunctions.AppGraphics;
 
@@ -16,6 +18,7 @@ public class FragmentStore extends Fragment implements View.OnClickListener {
 
     // Variables for Activity Fragment
     AppGraphics activityFragGraphics;
+    Button button_00, button_01, button_02;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -23,17 +26,35 @@ public class FragmentStore extends Fragment implements View.OnClickListener {
         View v = inflater.inflate(R.layout.fragment_store, container, false);
         activityFragGraphics = new AppGraphics(getActivity());
 
+        // Assigning UI attributes
+        button_00 = (Button) v.findViewById(R.id.store_00);
+        button_00.setOnClickListener(this);
+        button_00.setTextColor(Color.WHITE);
+        button_01 = (Button) v.findViewById(R.id.store_01);
+        button_01.setOnClickListener(this);
+        button_01.setTextColor(Color.WHITE);
+        button_02 = (Button) v.findViewById(R.id.store_02);
+        button_02.setOnClickListener(this);
+        button_02.setTextColor(Color.WHITE);
+
         return v;
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            /*
-            case R.id.citizen_button:
+            // To General Store
+            case R.id.store_00:
 
                 break;
-                */
+            // To Seed Machine
+            case R.id.store_01:
+
+                break;
+            // To Bunny Machine
+            case R.id.store_02:
+
+                break;
             default:
                 break;
         }
