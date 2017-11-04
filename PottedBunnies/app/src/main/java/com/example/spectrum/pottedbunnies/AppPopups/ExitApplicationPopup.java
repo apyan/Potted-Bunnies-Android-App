10f5aceb695a59b5e7567fb75145c7243c1d3e2c
Alvin <apyan7852@gmail.com>
2017-11-04 14:34:40 -0700

    App Clean Up
    Clean up the application files a bit.
    Also, arranged so it can be developed on Android Studio 3.0

---
+++ PottedBunnies/app/src/main/java/com/example/spectrum/pottedbunnies/AppPopups/ExitApplicationPopup.java
@@ -0,0 +1,13 @@
+package com.example.spectrum.pottedbunnies.AppPopups;
+
+import android.widget.PopupWindow;
+
+/**
+ * Created by apyan on 11/4/17.
+ */
+
+public class ExitApplicationPopup extends PopupWindow {
+
+
+
+}
--- PottedBunnies/app/src/main/java/com/example/spectrum/pottedbunnies/AppPopups/PopupAppExit.java
+++
@@ -1,1 +0,0 @@
-        appGraphics = new AppGraphics(context);
